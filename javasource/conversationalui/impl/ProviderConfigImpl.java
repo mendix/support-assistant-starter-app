04b748af841cc98e7b1dbab08613f0e94d2e4658
Liam Sommer <liam.sommer@mendix.com>
2024-07-29 13:49:45 +0000

    Initial app upload.

---
+++ javasource/conversationalui/impl/ProviderConfigImpl.java
@@ -0,0 +1,54 @@
+package conversationalui.impl;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Map;
+import com.mendix.core.Core;
+import com.mendix.systemwideinterfaces.core.IContext;
+import com.mendix.systemwideinterfaces.core.IDataType;
+import com.mendix.systemwideinterfaces.core.IMendixObject;
+import conversationalui.proxies.ChatContext;
+import conversationalui.proxies.ProviderConfig;
+
+public class ProviderConfigImpl{
+	
+	public static void validateActionMicroflow(String ActionMicroflow) {
+		if (ActionMicroflow == null || ActionMicroflow.isBlank()) {
+			throw new IllegalArgumentException("ActionMicroflow is required.");
+		}
+		
+		Map<String, IDataType> inputParameters = Core.getInputParameters(ActionMicroflow);
+		if (inputParameters == null || inputParameters.size() != 1) {
+			throw new IllegalArgumentException("ActionMicroflow " + ActionMicroflow + " should only have one input parameter of type " + ChatContext.getType() + ".");
+		}
+		
+		if(ChatContext.getType().equals(inputParameters.entrySet().iterator().next().getValue().getObjectType()) == false) {
+			throw new IllegalArgumentException("ActionMicroflow " + ActionMicroflow + " should have an input parameter of type " + ChatContext.getType()+ ".");			
+		}
+
+		if(Core.getReturnType(ActionMicroflow) == null || IDataType.DataTypeEnum.Boolean.equals(Core.getReturnType(ActionMicroflow).getType()) == false) {
+			throw new IllegalArgumentException("ActionMicroflow " + ActionMicroflow + " should have a Boolean return value.");		
+		}
+	}
+	
+	public static void validateSpecialization(IMendixObject ProviderConfigSpecialization) throws Exception {
+		requireNonNull(ProviderConfigSpecialization, "ProviderConfig Specialization is required.");
+		if (!ProviderConfigSpecialization.isInstanceOf(ProviderConfig.entityName)){
+			throw new IllegalArgumentException(ProviderConfig.entityName + " or a specialization of such is required.");
+		}		
+	}
+	
+	public static ProviderConfig createAndSetProviderConfigSpecialization(IContext context, String ProviderConfigSpecialization, String ActionMicroflow, String ProviderName) throws Exception {
+		// Create an instance of the specialized ProviderConfig object
+		IMendixObject providerConfigSpecialization = Core.instantiate(context, ProviderConfigSpecialization);
+		ProviderConfigImpl.validateSpecialization(providerConfigSpecialization);
+
+		// Use the specialized proxy class to wrap the generic IMendixObject to set attributes
+		ProviderConfig providerConfig = ProviderConfig.initialize(context, providerConfigSpecialization);
+		providerConfig.setActionMicroflow(ActionMicroflow);
+		if (ProviderName != null && !ProviderName.isBlank()) {
+			providerConfig.setDisplayName(ProviderName.trim());
+		}
+		return providerConfig;
+	}
+}
---
+++ javasource/pgvectorknowledgebase/impl/ChunkUtils.java
@@ -0,0 +1,101 @@
+package pgvectorknowledgebase.impl;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import com.mendix.core.Core;
+import com.mendix.systemwideinterfaces.core.IContext;
+import com.mendix.systemwideinterfaces.core.IMendixObject;
+import com.mendix.systemwideinterfaces.core.meta.IMetaAssociation;
+import com.mendix.systemwideinterfaces.core.meta.IMetaObject;
+
+import communitycommons.ORM;
+import pgvectorknowledgebase.proxies.Chunk;
+
+public class ChunkUtils {
+	
+	
+	public static void validateTargetChunk(IMetaObject TargetChunk) throws Exception {
+		// verify target chunk on non-null, subclass of chunk, 
+		requireNonNull(TargetChunk, "Target Chunk must be specified");
+		if (! TargetChunk.isSubClassOf(Chunk.entityName)){
+			throw new IllegalArgumentException("Target Chunk must be a specialization of " + Chunk.entityName);
+		}		
+	};
+
+
+
+
+	public static java.util.List<IMendixObject> getTargetChunkList(
+			IContext context, java.util.List<Chunk> chunkList, IMetaObject targetChunk) {
+		// create list to return
+		java.util.List<IMendixObject> targetChunkList = new ArrayList<IMendixObject>();
+		
+		// per chunk create a TargetChunk (custom specialization) 
+		chunkList.forEach(c -> {
+			// - instantiate Target Chunk (custom specialization)
+			IMendixObject targetChunkSpecialization = Core.instantiate(context, targetChunk.getName());
+			// copy values from Chunk to Target Chunk (custom specialization)
+			ORM.cloneObject(context, c.getMendixObject(), targetChunkSpecialization, true);
+			try {
+				// - retrieve Mendix target object    
+				String MxObjectID = c.getMxObjectID(context);
+				IMendixObject targetObject = MxObjectID == null ? null : Core.retrieveId(
+						context, Core.createMendixIdentifier(
+								MxObjectID
+								)
+						); 
+				
+				// find matching association based on meta object name 
+				Long assocationsSetCount= targetChunk
+						.getMetaAssociationsParent()
+						.stream()
+						.filter(a -> assocationMatchesTarget(a, targetObject)) 
+						.peek(a -> setAssociationToTarget(context, targetChunkSpecialization, targetObject, a))
+						.collect(Collectors.counting());
+				
+				// set association if found, otherwise log a warning
+				if (assocationsSetCount == 0){
+					LOGGER.warn("No eligible association found for target object " + targetObject.getMetaObject().getName()
+						+ " on entity " + targetChunk.getName());	
+				}
+				
+			} catch (Exception e) {
+				LOGGER.error(e.getMessage());
+			}
+			
+			targetChunkList.add(targetChunkSpecialization);
+					
+		});
+		return targetChunkList;
+	}
+
+	
+	
+	public static void addChunkWithMxObjectID(IContext context, IMendixObject MxObject, java.util.List<Chunk> chunkList) {
+		Chunk chunk = new Chunk(context);
+		chunk.setMxObjectID(context, String.valueOf(MxObject.getId().toLong()));
+		chunkList.add(chunk);
+	}
+
+	
+	
+	private static void setAssociationToTarget(IContext context, IMendixObject chunk,IMendixObject targetObject, IMetaAssociation association){
+		if (targetObject != null) {
+			chunk.setValue(context, association.getName(), targetObject.getId());
+		}
+	}
+
+	
+	
+	private static boolean assocationMatchesTarget(IMetaAssociation asssociation, IMendixObject targetObject){
+		return targetObject == null ? false : targetObject.getMetaObject().isSubClassOf(asssociation.getChild());
+	}
+	
+	
+	
+	private static final MxLogger LOGGER = new MxLogger(ChunkUtils.class);
+	
+}
