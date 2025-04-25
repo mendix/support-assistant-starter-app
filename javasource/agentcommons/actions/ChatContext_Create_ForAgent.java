1369a1c0a11edcee7410158e8addf71037d78c3a
Tom Warmerdam <tom.warmerdam@mendix.com>
2025-04-25 10:28:15 +0000

    SAS-1237: reimport agent commons

--- javasource/agentcommons/actions/ChatContext_Create_ForAgent.java
+++ javasource/agentcommons/actions/ChatContext_Create_ForAgent.java
@@ -20,8 +20,9 @@ import com.mendix.systemwideinterfaces.core.IMendixObject;
 import com.mendix.systemwideinterfaces.core.UserAction;
 
 /**
- * This action is a combination of 'Get Prompt' and 'New Chat'.
- * First the variables in the prompt are replaced with attributes from the ContextObject to get the final system prompt. Then a new chat context is created with a provider config that is associated to a deployed model. Additionally, the system prompt is set based on the Prompt and the action microflow of the new provider config is set.
+ * This action creates a ChatContext based on an Agent and optionally a ContextObject.
+ * 
+ * If a ContextObject is passed, the variables in the prompt are replaced with attributes from the ContextObject to get the final system prompt. Then a new chat context is created with a provider config that is associated to a deployed model as configured for the agent. Additionally, the system prompt is set based on the prompt of the agent. Finally, the action microflow of the new provider config is set.
  * 
  * If an internal error occurs, the operation will return empty and the error is logged.
  */
@@ -63,7 +64,7 @@ public class ChatContext_Create_ForAgent extends UserAction<IMendixObject>
 		try {
 		    requireNonNull(Agent, "Prompt is required.");
 		    DeployedModel deployedModel = OverwritingDeployedModel != null ? OverwritingDeployedModel : Agent.getAgent_DeployedModel();
-		    requireNonNull(deployedModel, "No DeployedModel could be used for creating the ChatContext. Either pass the OverwritingDeployedModel or make sure to use an Agent that has a DeployedModel associated.");
+		    requireNonNull(deployedModel, "No DeployedModel could be used for creating the ChatContext. Either pass the OverwritingDeployedModel or make sure to use a Prompt that has a DeployedModel associated.");
 		    
 		    IMendixObject returnValue = Core.userActionCall("AgentCommons." + PromptToUse_GetAndReplace.class.getSimpleName())
 		    		.withParams(Agent.getMendixObject(), ContextObject)
